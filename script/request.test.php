a807f194e5038404dd751f3749e7da37e60fe95f
Jimmy Latour <jimmy.eoxia@gmail.com>
2016-06-22 11:59:51 +0200

    Update request.test.php

--- script/request.test.php
+++ script/request.test.php
@@ -1,7 +1,21 @@
 <?php
 /**
-* @author: Jimmy Latour jimmy.eoxia@gmail.com
+* @author: Jimmy Latour lelabodudev@gmail.com
 */
+
+if ( !function_exists( 'search_files' ) ) {
+	function search_files($folder, $pattern) {
+		$dir = new RecursiveDirectoryIterator($folder);
+		$ite = new RecursiveIteratorIterator($dir);
+		$files = new RegexIterator($ite, $pattern, RegexIterator::GET_MATCH);
+		$fileList = array();
+		foreach($files as $file)
+		{
+			$fileList[] = $file[0];
+		}
+		return $fileList;
+	}
+}
 
 echo "[+] Starting Request Tests" . PHP_EOL . PHP_EOL;
 
@@ -12,44 +26,68 @@ $total_unsecured_line = 0;
 $pattern = '#\$_POST|\$_GET|\$_REQUEST#';
 
 // Loop on unitList
-foreach($unitList as $test)
+foreach ( $unitList as $file_url )
 {
-	echo "[+] Testing -> " . $test . PHP_EOL;
-  if ( $test != '../script/request.test.php' ) {
-    $file = file_get_contents( $test );
-    $string_post_unsecured[$test] = array();
-    $lines = explode( PHP_EOL, $file );
+	// echo "[+] Testing -> " . $file_url . PHP_EOL;
+  $file = file_get_contents( $file_url );
+  $string_post_unsecured[$file_url] = array();
+  $lines = explode( PHP_EOL, $file );
 
-    if ( !empty( $lines ) ) {
-      foreach ( $lines as $key => $line ) {
-        if ( preg_match( $pattern, $line ) ) {
-          $lines[$key] = preg_replace( '#!empty\(.+?(\$_POST|\$_GET|$_REQUEST)\[\'.+\'\].+?\) \?#isU', '', $lines[$key] );
-          if ( !preg_match( '#sanitize_.+#', $lines[$key] ) &&
-            !preg_match( '#\*#', $lines[$key] ) &&
-            !preg_match( '#\\/\/#', $lines[$key] ) &&
-            !preg_match( '#( +?int +?)#', $lines[$key] ) ) {
-              $string_post_unsecured[$test][$key + 1] = htmlentities( $lines[$key] );
-              $total_unsecured_line++;
-          }
+	if ( !empty( $lines ) ) {
+		foreach ( $lines as $key => $line ) {
+	    if ( preg_match( $pattern, $line ) ) {
+	      $lines[$key] = preg_replace( '#!empty\(.+?(\$_POST|\$_GET|\$_REQUEST)\[\'.+\'\].+?\) \?#isU', '', $lines[$key] );
 
-          if ( preg_match( '#(\$_POST|\$_GET|\$_REQUEST)\[\'.+\'\].+?\=#isU', $lines[$key] ) ) {
-            $string_post_unsecured[$test][$key + 1] = htmlentities( $lines[$key] );
-            $total_unsecured_line++;
-          }
-        }
-      }
-    }
+			if ( basename( $file_url ) != "../task-manager/script/request.test.php" ) {
+			  if ( !preg_match( '#sanitize_.+#', $lines[$key] ) &&
+        !preg_match( '#esc_.+#', $lines[$key] ) &&
+				!preg_match( '#\*#', $lines[$key] ) &&
+				!preg_match( '#\\/\/#', $lines[$key] ) &&
+				!preg_match( '#\( ?int ?\)#', $lines[$key] ) &&
+				!preg_match( '#\( ?array ?\)#', $lines[$key] ) &&
+				!preg_match( '#\( ?float ?\)#', $lines[$key] ) &&
+				!preg_match( '#\( ?bool ?\)#', $lines[$key] ) &&
+				!preg_match( '#intval#', $lines[$key] ) &&
+				!preg_match( '#varSanitizer#', $lines[$key] ) ) {
+				  $string_post_unsecured[$file_url][$key + 1] = htmlentities( $lines[$key] );
+				  $total_unsecured_line++;
+			  }
+
+			  if ( preg_match( '#(\$_POST|\$_GET|\$_REQUEST)\[\'.+\'\].+?\=#isU', $lines[$key] ) &&
+        !preg_match( '#\* @#', $lines[$key] ) &&
+        !preg_match( '#\\/\/#', $lines[$key] ) &&
+        !preg_match( '#\*#', $lines[$key] ) ) {
+  				$string_post_unsecured[$file_url][$key + 1] = htmlentities( $lines[$key] );
+  				$total_unsecured_line++;
+			  }
+			}
+		}
+	}
   }
 }
 
 echo "[+] Total unsecured line : " . $total_unsecured_line . PHP_EOL . '<br />';
 
 if ( !empty( $string_post_unsecured ) ) {
-  foreach ( $string_post_unsecured as $name_file => $file ) {
+  foreach ( $string_post_unsecured as $file_url => $file ) {
     if ( !empty( $file ) ) {
-      echo "[+] File : " . $name_file . ' => Unsecured $_POST|$_GET|$_REQUEST ' . count( $file ) . PHP_EOL . '<br />';
+      echo "[+] File : " . $file_url . ' => Unsecured $_POST|$_GET|$_REQUEST ' . count( $file ) . PHP_EOL . '<br />';
       foreach ( $file as $line => $content ) {
-        echo "[+] Line : " . $line . " => " . trim($content) . PHP_EOL;
+        $color = "black";
+        if ( preg_match( '#\$_POST#', trim($content) ) ) {
+          $color = "#ea6153";
+        }
+        else if( preg_match( '#\$_GET#', trim($content) ) ) {
+          $color = "#3498db";
+        }
+        else if( preg_match( '#\$_REQUEST#', trim($content) ) ) {
+          $color = "#2ecc71";
+        }
+        else if( preg_match( '#\$_SESSION#', trim($content) ) ) {
+          $color = "#f1c40f";
+        }
+
+        echo "[+] <span style='color: " . $color . "'>Line : " . $line . " => " . trim($content) . PHP_EOL . '</span><br />';
       }
     }
   }
