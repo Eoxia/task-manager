91a27121d2d3fbeb154b6aa4cdd7bc0833112f0f
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-06 17:22:17 +0200

    Update value in input due_date

--- core/task/template/backend/window/action.php
+++ core/task/template/backend/window/action.php
@@ -28,7 +28,7 @@
 			<form action="<?php echo admin_url('admin-ajax.php'); ?>" method="POST">
 				 <!-- For admin-ajax -->
 			 	<?php wp_nonce_field( 'ajax_update_due_date_' . $element->id ); ?>
-				<input type="text" class="isDate" name="due_date" value="<?php echo !empty( $element->option['date_info']['due'] ) ? $element->option['date_info']['due'] : current_time( 'Y-m-d' ); ?>" />
+				<input type="text" class="isDate" name="due_date" value="<?php echo !empty( $element->option['date_info']['due'] ) && !empty( max( $element->option['date_info']['due'] ) ) ? max( $element->option['date_info']['due'] )['date'] : current_time( 'Y-m-d' ); ?>" />
 				<input type="hidden" name="task_id" value="<?php echo $element->id; ?>" />
 				<input type="button" class="wpeo-update-due-date" value="<?php _e( 'Update', 'task-manager' ); ?>" />
 			</form>
