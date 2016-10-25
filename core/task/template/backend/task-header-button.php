02d3f47cf830a7c18eb3d6a52218e29014588eb1
amandine-eoxia <amandine@eoxia.com>
2016-10-25 14:20:34 +0200

    css bulles de couleurs

--- core/task/template/backend/task-header-button.php
+++ core/task/template/backend/task-header-button.php
@@ -1,6 +1,9 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit; ?>
 
 <ul>
+	<li class="task-color">
+		<span class="white"></span><span class="red"></span><span class="yellow"></span><span class="green"></span><span class="blue"></span><span class="purple"></span>
+	</li>
 	<li>
 		<span class="wpeo-send-task-to-trash" data-nonce="<?php echo wp_create_nonce( 'wpeo_nonce_delete_task_' . $task->id ); ?>"><?php _e( 'Delete task', 'task-manager' ); ?></span>
 	</li>
