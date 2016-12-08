190c20ac4a1d4f6fbe8b149da9609d88455257a3
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-12-08 17:08:12 +0100

    6040 - Vérifier si SSL est activé pour les gravatars

--- module/user/template/backend/user-gravatar.php
+++ module/user/template/backend/user-gravatar.php
@@ -2,12 +2,12 @@
 
 <?php if ( !empty( $user->id ) ): ?>
 	<li title="<?php echo $user->email; ?>" style="background-color: #<?php echo $user->option['user_info']['avatar_color']; ?>; width: <?php echo !empty( $size ) ? $size : 50; ?>px; height: <?php echo !empty( $size ) ? $size : 50; ?>px;" class='user <?php echo !empty( $active ) ? $active : ''; ?>  wpeo-user-<?php echo $user->id; ?>' data-id="<?php echo $user->id; ?>" <?php echo !empty( $nonce ) ? "data-nonce='" . wp_create_nonce( $nonce ) . "'" : ''; ?>>
-		<img src="<?php echo $user->option['user_info']['avatar']; ?>?s=<?php echo !empty( $size ) ? $size : 50; ?>&d=blank" />
+		<?php echo get_avatar( $user->id, 50, 'blank' ); ?>
 		<div class="wpeo-avatar-initial"><span><?php echo strtoupper( $user->option['user_info']['initial'] ); ?></span></div>
 	</li>
 	<?php if (!empty( $display_name ) ): ?><li class="name"><span><?php echo $user->displayname; ?></span></li><?php endif; ?>
 <?php else: ?>
 	<li>
-		<img src="http://www.gravatar.com/avatar/00000000000000000000000000000000?s=<?php echo !empty( $size ) ? $size : 50; ?>" />
+		<?php echo get_avatar( 0, 50 ); ?>
 	</li>
 <?php endif; ?>
