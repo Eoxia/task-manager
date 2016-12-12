108d66da49b90d6bdc3b89c037f7d70a86f94922
Gaël Gaudy <gael.eoxia@gmail.com>
2016-12-12 15:35:19 +0100

    Seul la dernière tâche ouverte en mode édition d'utilisateur se ferme lors du clic

--- module/user/asset/js/backend.js
+++ module/user/asset/js/backend.js
@@ -1,13 +1,12 @@
-/** wpeo_users/asset/js/backend.js wpeo_user */
+/** Wpeo_users/asset/js/backend.js wpeo_user */
 
-jQuery(document).ready(function() {
+jQuery( document ).ready(function() {
 	wpeo_user.init();
 });
 
 var wpeo_user = {
-	list_user_open: false,
+	List_user_open: false,
 	list_user_owner_open: false,
-	current_task_id: 0,
 	list_user_id: [],
 
 	init: function() {
@@ -20,14 +19,7 @@ var wpeo_user = {
 		jQuery( document ).on( 'click', '.wpeo-edit-select-user li', function( event ) { wpeo_user.select_user( event, jQuery( this ) ); } );
 		jQuery( document ).on( 'click', '.wpeo-main-user .wpeo-confirm-user', function( event ) { wpeo_user.hide_no_active( event ); } );
 
-		jQuery( document ).on( 'click', 'body', function( event ) {
-			if( wpeo_user.current_task_id != 0 && wpeo_user.list_user_open ) {
-				wpeo_user.hide_no_active( event );
-			}
-			else if( wpeo_user.current_task_id != 0 && wpeo_user.list_user_owner_open ) {
-				wpeo_user.close_list_owner( event );
-			}
-		} );
+		jQuery( document ).on( 'click', 'body', function( event ) { wpeo_user.hide_no_active(); wpeo_user.close_list_owner(); } );
 
 		/** Chosen */
 		jQuery( '.wpeo-user-filter' ).chosen().change( function( evt, params ) { wpeo_user.change_filter( evt, params ); } );
@@ -47,9 +39,9 @@ var wpeo_user = {
 	open_list: function( event, element ) {
 		var bloc_user = jQuery( element ).closest( '.wpeo-bloc-user' );
 		var ul_user	= bloc_user.find( '.wpeo-ul-user' );
-		wpeo_user.close_list_owner( event );
 		var object_id = bloc_user.data( 'id' );
-		wpeo_user.current_task_id = object_id;
+		wpeo_user.close_list_owner();
+		wpeo_user.hide_no_active();
 		wpeo_user.list_user_open = true;
 
 		bloc_user.bgLoad();
@@ -60,7 +52,7 @@ var wpeo_user = {
 		var data = {
 			action: 'wpeo-view-user',
 			object_id: object_id,
-			_wpnonce: jQuery( element ).data( 'nonce' ),
+			_wpnonce: jQuery( element ).data( 'nonce' )
 		};
 
 		/** Animation **/
@@ -88,41 +80,40 @@ var wpeo_user = {
 			object_id: object_id,
 			user_id: user_id,
 			selected: jQuery( element ).hasClass( 'active' ),
-			_wpnonce: jQuery( element ).data( 'nonce' ),
+			_wpnonce: jQuery( element ).data( 'nonce' )
 		};
 
 		jQuery.eoajax( ajaxurl, data, function() {
 			if ( this.affected_to_task ) {
 				bloc_user.closest( '.wpeo-project-task' ).addClass( 'wpeo-affected-task' );
-			}
-			else {
+			} else {
 				bloc_user.closest( '.wpeo-project-task' ).removeClass( 'wpeo-affected-task' );
 			}
 		} );
 	},
 
-	hide_no_active: function( event ) {
-		if( wpeo_user.list_user_open ) {
-			event.preventDefault();
+	hide_no_active: function() {
+		if ( wpeo_user.list_user_open ) {
+			jQuery( '.wpeo-edit-select-user' ).each( function( index, element ) {
+				var bloc_user = jQuery( element ).closest( '.wpeo-bloc-user' );
+				//Var bloc_user = jQuery( '.wpeo-project-task[data-id="' + wpeo_user.current_task_id + '"] .wpeo-bloc-user' );
 
-			var bloc_user = jQuery( '.wpeo-project-task[data-id="' + wpeo_user.current_task_id + '"] .wpeo-bloc-user' );
+				bloc_user.find( '.wpeo-ul-user li' ).removeClass( 'fadeInRightBig' ).addClass( 'bounceOutRight' );
 
-			bloc_user.find( '.wpeo-ul-user li' ).removeClass('fadeInRightBig').addClass( 'bounceOutRight' );
+				setTimeout( function() {
+				 	bloc_user.find( '.wpeo-ul-user li:not(.active)' ).remove();
+				 	bloc_user.find( '.wpeo-ul-user li' ).removeClass( 'bounceOutRight' ).addClass( 'fadeInRightBig' );
+				 }, 500, function() {
+				 	bloc_user.find( '.wpeo-ul-user li' ).removeClass( 'fadeInRightBig' );
+				 } );
 
-			setTimeout( function() {
-			 	bloc_user.find( '.wpeo-ul-user li:not(.active)' ).remove();
-			 	bloc_user.find( '.wpeo-ul-user li' ).removeClass( 'bounceOutRight' ).addClass( 'fadeInRightBig' );
-			 }, 500, function() {
-			 	bloc_user.find( '.wpeo-ul-user li' ).removeClass( 'fadeInRightBig' );
-			 } );
+				bloc_user.find( '.wpeo-confirm-user' ).removeClass( 'wpeo-confirm-user' );
+				bloc_user.find( '.wpeo-edit-select-user' ).removeClass( 'wpeo-edit-select-user' );
 
-			bloc_user.find( '.wpeo-confirm-user' ).removeClass( 'wpeo-confirm-user' );
-			bloc_user.find( '.wpeo-edit-select-user' ).removeClass( 'wpeo-edit-select-user' );
-
-			bloc_user.find( '.dashicons-edit' ).toggleClass( 'dashicons-plus dashicons-edit' );
-			wpeo_user.current_task_id = 0;
-			wpeo_user.list_user_open = false;
+				bloc_user.find( '.dashicons-edit' ).toggleClass( 'dashicons-plus dashicons-edit' );
+			} );
 		}
+		wpeo_user.list_user_open = false;
 	},
 
 	/**
@@ -133,11 +124,11 @@ var wpeo_user = {
 	open_list_owner: function( event, element ) {
 		var bloc_user 	= jQuery( element ).closest( '.wpeo-bloc-user' );
 		var task_id 	= bloc_user.data( 'id' );
-		wpeo_user.hide_no_active( event );
-		wpeo_user.current_task_id = task_id;
+		wpeo_user.hide_no_active();
+		wpeo_user.close_list_owner();
 
 		/** On rajoute une class à l'objet pour ne pas reéxécuter cette requête AJAX */
-		bloc_user.find( '.wpeo-user-owner' ).addClass( 'wpeo-confirm-user' );
+		bloc_user.find( '.wpeo-user-owner' ).addClass( 'wpeo-confirm-user wpeo-edit-select-owner' );
 
 		var data = {
 			action: 'wpeo-render-edit-owner-user',
@@ -165,21 +156,19 @@ var wpeo_user = {
 
 		jQuery.eoajax( ajaxurl, data, function() {
 			 bloc_user.find( '.wpeo-user-owner' ).html( bloc_user.find( '.wpeo-user-owner li.wpeo-user-' + this.owner_id ) );
-			 bloc_user.find( '.wpeo-user-owner' ).removeClass( 'wpeo-confirm-user' );
-			 wpeo_user.current_task_id = 0;
-			 wpeo_user.list_user_owner_open = false;
+			 wpeo_user.close_list_owner();
 		} );
 	},
 
-	close_list_owner: function( event ) {
+	close_list_owner: function() {
 		if( wpeo_user.list_user_owner_open) {
-			var bloc_user = jQuery( '.wpeo-project-task[data-id="' + wpeo_user.current_task_id + '"] .wpeo-bloc-user' );
-
-			bloc_user.find( '.wpeo-user-owner' ).html( bloc_user.find( '.wpeo-user-owner li:first' ) );
-			bloc_user.find( '.wpeo-user-owner' ).removeClass( 'wpeo-confirm-user' );
-			wpeo_user.current_task_id = 0;
-			wpeo_user.list_user_owner_open = false;
+			jQuery( '.wpeo-edit-select-owner' ).each( function( index, element ) {
+				var bloc_user 	= jQuery( element ).closest( '.wpeo-bloc-user' );
+				bloc_user.find( '.wpeo-user-owner' ).html( bloc_user.find( '.wpeo-user-owner li:first' ) );
+				bloc_user.find( '.wpeo-user-owner' ).removeClass( 'wpeo-confirm-user' );
+			} );
 		}
+		wpeo_user.list_user_owner_open = false;
 	},
 
 	change_filter: function( event, params ) {
