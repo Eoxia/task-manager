6ce61ca70816cb5b5c36766185c2e0321ed7b570
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-03-21 14:23:26 +0100

    Ajout de current_user_can

--- core/time/template/backend/time.php
+++ core/time/template/backend/time.php
@@ -15,7 +15,7 @@
 	<li class="action">
 		<?php echo $time->option['time_info']['elapsed']; ?>
 		<span class="dashicons dashicons-clock"></span>
-		<?php if ( is_admin() ): ?>
+		<?php if ( current_user_can( 'administrator' ) ): ?>
 			<span data-nonce="<?php echo wp_create_nonce( 'wpeo_nonce_get_point_time_' . $time->id ); ?>" class="dashicons dashicons-edit wpeo-point-time-edit-btn"></span>
 			<span data-nonce="<?php echo wp_create_nonce( 'wpeo_nonce_delete_point_time_' . $time->id ); ?>" class="dashicons dashicons-no wpeo-send-point-time-to-trash"></span>
 		<?php endif; ?>
