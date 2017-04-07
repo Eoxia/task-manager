b6f2e5b4e405d3bd25d95f7665c542d58a9bea80
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-04-07 09:27:24 +0200

    Ajout des derniers tickets

--- core/taskmanager/taskmanager.action.01.php
+++ core/taskmanager/taskmanager.action.01.php
@@ -7,6 +7,10 @@ if( !class_exists( 'taskmanager_action_01' ) ) {
 
 		public function __construct() {
       add_action( 'wp_ajax_search', array( $this, 'ajax_search' ) );
+
+			add_action( 'admin_bar_menu', array( $this, 'callback_admin_bar_menu' ), 106 );
+
+			add_action( 'wp_ajax_open_popup_last_wpshop_customer_comment', array( $this, 'callback_open_popup_last_wpshop_customer_comment' ) );
 		}
 
     public function ajax_search() {
@@ -33,7 +37,105 @@ if( !class_exists( 'taskmanager_action_01' ) ) {
       }
       wp_die( wp_json_encode( $return ) );
     }
-  }
+
+			/**
+		 * Permet d'afficher la dashicons qui vas être affiché dans la barre de WordPress.
+		 *
+		 * @param mixed $wp_admin_bar L'objet de WordPress pour gérer les noeuds.
+		 *
+		 * @return void
+		 *
+		 * @since 1.0.1.0
+		 * @version 1.0.1.0
+		 */
+		public function callback_admin_bar_menu( $wp_admin_bar ) {
+			if ( current_user_can( 'administrator' ) ) {
+				global $wpdb;
+				$query_args = array(
+					'action' => 'open_popup_last_wpshop_customer_comment',
+					'width' => '1000',
+					'height' => '900',
+				);
+				$comments = $wpdb->get_results(
+					"SELECT TIME.comment_date
+					FROM $wpdb->comments as TIME
+						JOIN $wpdb->comments AS POINT ON TIME.comment_parent=POINT.comment_ID
+						JOIN $wpdb->posts AS TASK 		ON POINT.comment_post_id=TASK.id
+						JOIN $wpdb->commentmeta AS POINTMETA ON POINT.comment_ID=POINTMETA.comment_id
+						JOIN $wpdb->users AS USER ON TIME.user_id=USER.ID
+						JOIN $wpdb->usermeta AS USERMETA ON USER.ID=USERMETA.user_id
+					WHERE TASK.post_type='wpeo-task'
+						AND	POINTMETA.meta_key='wpeo_point'
+						AND POINTMETA.meta_value LIKE '%completed\":false%'
+						AND TIME.comment_content != ''
+						AND TIME.comment_approved != 'trash'
+						AND USERMETA.meta_key='wp_user_level'
+						AND USERMETA.meta_value=0
+					ORDER BY TIME.comment_date DESC"
+				);
+				$current_date = current_time( 'timestamp' );
+				$new_comment = '';
+				if ( ! empty( $comments ) ) {
+					foreach ( $comments as $comment ) {
+						$timestamp_comment = mysql2date( 'U', $comment->comment_date );
+						if ( ( $current_date - ( 3600 * 24 ) * 5 ) < $timestamp_comment ) {
+							$new_comment = '🔴';
+							break;
+						}
+					}
+				}
+				$href = add_query_arg( $query_args, admin_url( 'admin-ajax.php' ) );
+				$button_open_popup = array(
+					'id'       	=> 'button-open-popup-last-comment-customer',
+					'href'			=> '#',
+					'title'    	=> $new_comment . count( $comments ) . ' tickets',
+					'meta'		 	=> array(
+						'onclick' => 'tb_show( "Les derniers commentaires des clients WPShop", "' . $href . '")',
+					),
+				);
+				$wp_admin_bar->add_node( $button_open_popup );
+			}
+		}
+
+		/**
+	 * Cette méthode appelle une vue qui sera renvoyé au rendu de la thickbox.
+	 * Elle charge également les 5 derniers commentaires des clients de WPShop.
+	 *
+	 * @return void
+	 *
+	 * @since 1.0.1.0
+	 * @version 1.0.1.0
+	 */
+	public function callback_open_popup_last_wpshop_customer_comment() {
+		global $time_controller, $wpdb;
+		$comments = $wpdb->get_results(
+			"SELECT TIME.comment_ID, TIME.comment_content, TASK.post_parent, USER.user_email, TIME.comment_date FROM $wpdb->comments as TIME
+			JOIN $wpdb->comments AS POINT
+			ON TIME.comment_parent=POINT.comment_ID
+			JOIN $wpdb->posts AS TASK
+			ON POINT.comment_post_id=TASK.id
+			JOIN $wpdb->commentmeta AS POINTMETA
+			ON POINT.comment_ID=POINTMETA.comment_id
+			JOIN $wpdb->users AS USER
+			ON TIME.user_id=USER.ID
+			JOIN $wpdb->usermeta AS USERMETA
+			ON USER.ID=USERMETA.user_id
+			WHERE TASK.post_type='wpeo-task' AND
+			POINTMETA.meta_key='wpeo_point' AND
+			POINTMETA.meta_value LIKE '%completed\":false%' AND
+			TIME.comment_content != '' AND
+			TIME.comment_approved != 'trash' AND
+			USERMETA.meta_key='wp_user_level' AND
+			USERMETA.meta_value=0
+			ORDER BY TIME.comment_date DESC
+			LIMIT 0, 10"
+		);
+		ob_start();
+		require( WPEO_TASKMANAGER_PATH . 'core/template/backend/popup.view.php' );
+		wp_die( ob_get_clean() ); // WPCS: XSS is ok.
+	}
+
+	}
 
 	global $taskmanager_action;
 	$taskmanager_action = new taskmanager_action_01();
