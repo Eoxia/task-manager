03a3b440d57158ace832c377a7184b776c1c61ad
amandine-eoxia <amandine@eoxia.com>
2017-04-18 17:56:42 +0200

    Css en cours d'actualisation

--- module/point/view/backend/point.view.php
+++ module/point/view/backend/point.view.php
@@ -21,24 +21,26 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 		<input type="hidden" name="action" value="edit_point" />
 		<input type="hidden" name="id" value="<?php echo esc_attr( $point->id ); ?>" />
 		<input type="hidden" name="parent_id" value="<?php echo esc_attr( $parent_id ); ?>" />
-		<ul>
-			<li>
+		<ul class="point-container">
+			<li class="point-valid">
 				<?php if ( ! empty( $point->id ) ) : ?>
-					<span class="dashicons dashicons-screenoptions" title="<?php esc_attr_e( 'Drag and drop for set the order', 'task-manager' ); ?>"></span>
+					<span class="wpeo-sort-point dashicons dashicons-screenoptions" title="<?php esc_attr_e( 'Drag and drop for set the order', 'task-manager' ); ?>"></span>
 					<input type="checkbox" <?php echo ! empty( $point->point_info['completed'] ) ? 'checked': ''; ?> class="completed-point" data-nonce="<?php echo esc_attr( wp_create_nonce( 'complete_point' ) ); ?>" />
+				<?php endif; ?>
+			</li>
 
+			<li class="point-content content">
+				<?php if ( ! empty( $point->id ) ) : ?>
 					<span data-action="<?php echo esc_attr( 'load_comments' ); ?>"
 								data-task-id="<?php echo esc_attr( $parent_id ); ?>"
 								data-point-id="<?php echo esc_attr( $point->id ); ?>"
 								data-module="comment"
 								data-before-method="beforeLoadComments"
-								class="dashicons dashicons-arrow-right-alt2 action-attribute"></span>
+								class="animated dashicons dashicons-arrow-right-alt2 action-attribute"></span>
 
 					<span class="wpeo-block-id">#<?php echo esc_attr( $point->id ); ?></span>
 				<?php endif; ?>
-			</li>
 
-			<li class="content">
 				<input type="hidden" name="content" value="<?php esc_attr( $point->content ); ?>" />
 				<div class="wpeo-point-new-contenteditable" contenteditable="true">
 					<?php echo esc_html( stripslashes( $point->content ) ); ?>
@@ -47,7 +49,7 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 					<span class="wpeo-point-new-placeholder"><?php esc_html_e( 'Write your point here...', 'task-manager' ); ?></span>
 				<?php endif; ?>
 			</li>
-			<li>
+			<li class="point-action">
 				<?php	if ( empty( $point->id ) ) : ?>
 					<div 	class="wpeo-point-new-btn action-input"
 								data-parent="form"
@@ -58,15 +60,17 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 				<?php else : ?>
 					<div class="hidden submit-form" data-parent="form"></div>
 
-					<span class="dashicons dashicons-clock"></span>
-					<span class="wpeo-time-in-point"><?php echo esc_attr( $point->time_info['elapsed'] ); ?></span>
+					<div class="wpeo-point-time">
+						<span class="dashicons dashicons-clock"></span>
+						<span class="wpeo-time-in-point"><?php echo esc_attr( $point->time_info['elapsed'] ); ?></span>
+					</div>
 
 					<div class="toggle wpeo-task-setting"
 							data-parent="point"
 							data-target="content">
 
 						<div class="action">
-							<span class="wpeo-task-open-action" title="<?php esc_html_e( 'Task options', 'task-manager' ); ?>"><i class="fa fa-ellipsis-v"></i></span>
+							<span class="wpeo-task-open-action" title="<?php esc_html_e( 'Task options', 'task-manager' ); ?>"><i class="fa fa-cog"></i></span>
 						</div>
 
 						<ul class="content point-header-action">
