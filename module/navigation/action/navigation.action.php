14f1760ad4ec3bc79fbeedbed9aa86de0abf5356
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-10-02 09:48:14 +0200

    1.4.0: Affiches maintenant correctement 'Chargement de tâche supplémentaire'

--- module/navigation/action/navigation.action.php
+++ module/navigation/action/navigation.action.php
@@ -116,7 +116,7 @@ class Navigation_Action {
 		$search_result_view = ob_get_clean();
 
 		ob_start();
-		do_shortcode( '[task users_id="' . $follower_id_selected . '" categories_id="' . $categories_id_selected . '" term="' . $term . '" posts_per_page="' . \eoxia\Config_Util::$init['task-manager']->task->posts_per_page . '"]' );
+		do_shortcode( '[task users_id="' . $follower_id_selected . '" categories_id="' . $categories_id_selected . '" term="' . $term . '" posts_per_page="' . \eoxia\Config_Util::$init['task-manager']->task->posts_per_page . '" with_wrapper="0"]' );
 		$tasks_view = ob_get_clean();
 
 		wp_send_json_success( array(
