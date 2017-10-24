90fc8613d0c12f367a4bcf3ead5056a5ce52c4ca
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-10-24 16:35:22 +0200

    1.5.0: Mise à la norme WordPress

--- module/follower/view/backend/follower.view.php
+++ module/follower/view/backend/follower.view.php
@@ -5,13 +5,15 @@
  * @package Task Manager
  * @subpackage Module/Follower
  *
- * @since 1.0.0.0
- * @version 1.3.6.0
+ * @since 1.0.0
+ * @version 1.5.0
  */
 
 namespace task_manager;
 
-if ( ! defined( 'ABSPATH' ) ) {	exit; } ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 
 <li class="follower active" style="width: 50px; height: 50px;">
 	<?php echo do_shortcode( '[task_avatar ids="' . $user->id . '" "size="50"]' ); ?>
