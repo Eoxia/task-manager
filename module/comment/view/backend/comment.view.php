0039a82026815185037c2972f7d11530a0b724dd
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-10-27 11:30:10 +0200

    1.5.0: Gestion des slashes

--- module/comment/view/backend/comment.view.php
+++ module/comment/view/backend/comment.view.php
@@ -3,16 +3,17 @@
  * Un commentaire dans le backend.
  *
  * @author Jimmy Latour <jimmy.eoxia@gmail.com>
- * @since 1.0.0.0
- * @version 1.3.6.0
+ * @since 1.0.0
+ * @version 1.5.0
  * @copyright 2015-2017 Eoxia
- * @package comment
- * @subpackage view
+ * @package Task_Manager
  */
 
 namespace task_manager;
 
-if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 
 <li class="comment view">
 	<ul class="wpeo-comment-container">
@@ -47,6 +48,6 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 				</ul>
 			</div>
 		</li>
-		<li class="wpeo-comment-content"><?php echo nl2br( stripslashes( $comment->content ) ); ?></li>
+		<li class="wpeo-comment-content"><?php echo nl2br( $comment->content ); ?></li>
 	</ul>
 </li>
