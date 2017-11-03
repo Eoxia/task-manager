acfa7f2692c635af3edf834b4ffa9dc688309ab7
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-03 09:58:37 +0100

    1.5.0: Affichage rapide des commentiares

--- module/comment/view/backend/list-comment.view.php
+++ module/comment/view/backend/list-comment.view.php
@@ -3,22 +3,24 @@
  * La liste des commentaires dans le backend.
  *
  * @author Jimmy Latour <jimmy.eoxia@gmail.com>
- * @since 1.0.0.0
- * @version 1.3.6.0
+ * @since 1.0.0
+ * @version 1.5.0
  * @copyright 2015-2017 Eoxia
- * @package comment
- * @subpackage view
+ * @package Task_Manager
  */
 
 namespace task_manager;
 
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 if ( ! empty( $comments ) ) :
 	foreach ( $comments as $comment ) :
 		if ( 0 !== $comment->id ) :
 			\eoxia\View_Util::exec( 'task-manager', 'comment', 'backend/comment', array(
 				'comment' => $comment,
+				'comment_selected_id' => $comment_selected_id,
 			) );
 		endif;
 	endforeach;
