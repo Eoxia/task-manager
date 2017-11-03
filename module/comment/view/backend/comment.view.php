db42782c9865de27c151ec658e645aa9fdd5cb8b
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-03 09:58:26 +0100

    1.5.0: Ajout d'affichage rapide

--- module/comment/view/backend/comment.view.php
+++ module/comment/view/backend/comment.view.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } ?>
 
-<li class="comment view">
+<li class="comment view <?php echo ( $comment_selected_id === $comment->id ) ? 'blink' : ''; ?>">
 	<ul class="wpeo-comment-container">
 		<li class="avatar"><?php echo do_shortcode( '[task_avatar ids="' . $comment->author_id . '" size="20"]' ); ?></li>
 		<li class="wpeo-comment-date"><?php echo esc_html( $comment->date['date_human_readable'] ); ?></li>
