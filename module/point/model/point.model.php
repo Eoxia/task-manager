34a7b8f24531b5ddbf5040144cacfbc5998406f5
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2018-01-03 17:14:40 +0100

    1.6.0: Mise à la norme WordPress

--- module/point/model/point.model.php
+++ module/point/model/point.model.php
@@ -4,8 +4,8 @@
  *
  * @author Jimmy Latour <jimmy.eoxia@gmail.com>
  * @since 1.0.0
- * @version 1.5.0
- * @copyright 2015-2017 Eoxia
+ * @version 1.6.0
+ * @copyright 2015-2018 Eoxia
  * @package Task_Manager
  */
 
@@ -24,49 +24,65 @@ class Point_Model extends \eoxia\Comment_Model {
 	 * Le Constructeur
 	 *
 	 * @since 1.0.0
-	 * @version 1.5.0
+	 * @version 1.6.0
 	 *
-	 * @param array  $object Les données reçu par WordPress.
+	 * @param array $object Les données reçu par WordPress.
 	 */
 	public function __construct( $object ) {
-		$this->model['status']['bydefault'] = '-34070';
+		$this->model['status']['bydefault']    = '-34070';
 		$this->model['author_id']['bydefault'] = get_current_user_id();
-		$this->model['date']['bydefault'] = current_time( 'mysql' );
+		$this->model['date']['bydefault']      = current_time( 'mysql' );
 
-		$this->model = array_merge( $this->model, array(
-			'time_info' => array(
-				'type' => 'array',
-				'meta_type' => 'multiple',
-				'bydefault' => array(
-					'elapsed' => 0,
-					'completed_point' => array(),
-					'uncompleted_point' => array(),
-				),
-				'elapsed' => array(
-					'type' => 'integer',
-					'meta_type' => 'multiple',
-				),
-				'completed_point' => array(
-					'type' => 'array',
-					'meta_type' => 'multiple',
-				),
-				'uncompleted_point' => array(
-					'type' => 'array',
-					'meta_type' => 'multiple',
-				),
+		$this->model['time_info'] = array(
+			'type'      => 'array',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+			'bydefault' => array(
+				'elapsed'           => array(),
+				'completed_point'   => array(),
+				'uncompleted_point' => array(),
 			),
-			'point_info' => array(
-				'type' => 'array',
-				'meta_type' => 'multiple',
-				'bydefault' => array(
-					'completed' => false,
-				),
-				'completed' => array(
-					'type' => 'boolean',
-					'meta_type' => 'multiple',
-				),
+		);
+
+		$this->model['time_info']['elapsed'] = array(
+			'type'       => 'array',
+			'array_type' => 'integer',
+			'meta_type'  => 'multiple',
+			'since'      => '1.0.0',
+			'version'    => '1.6.0',
+		);
+
+		$this->model['time_info']['completed_point'] = array(
+			'type'      => 'array',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
+
+		$this->model['time_info']['uncompleted_point'] = array(
+			'type'      => 'array',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
+
+		$this->model['point_info'] = array(
+			'type'      => 'array',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+			'bydefault' => array(
+				'completed' => false,
 			),
-		) );
+		);
+
+		$this->model['point_info']['completed'] = array(
+			'type'      => 'boolean',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
 
 		parent::__construct( $object );
 	}
