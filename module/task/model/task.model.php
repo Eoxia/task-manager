451583ab94b9c2022eb43da3b24259859156a01e
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2018-01-03 17:03:46 +0100

    1.6.0: Mise à la norme WordPress

--- module/task/model/task.model.php
+++ module/task/model/task.model.php
@@ -4,7 +4,7 @@
  *
  * @author Jimmy Latour <jimmy.eoxia@gmail.com>
  * @since 1.0.0
- * @version 1.4.0-ford
+ * @version 1.6.0
  * @copyright 2015-2017 Eoxia
  * @package Task_Manager
  */
@@ -23,95 +23,139 @@ class Task_Model extends \eoxia\Post_Model {
 	/**
 	 * Le constructeur défini le schéma.
 	 *
+	 * @since 1.0.0
+	 * @version 1.6.0
+	 *
 	 * @param Task_Model $object L'objet.
 	 */
 	public function __construct( $object ) {
-		$this->model = array_merge( $this->model, array(
-			'user_info' => array(
-				'type' => 'array',
-				'meta_type' => 'multiple',
-				'bydefault' => array(
-					'owner_id' => get_current_user_id(),
-					'affected_id' => array(),
-				),
-				'owner_id' => array(
-					'type' => 'integer',
-					'meta_type' => 'multiple',
-				),
-				'affected_id' => array(
-					'type' => 'array',
-					'meta_type' => 'multiple',
-				),
-				'writer_id' => array(
-					'type' => 'integer',
-					'meta_type' => 'multiple',
-				),
+		$this->model['user_info'] = array(
+			'type'      => 'array',
+			'meta_type' => 'multiple',
+			'bydefault' => array(
+				'owner_id'    => get_current_user_id(),
+				'affected_id' => array(),
 			),
-			'time_info' => array(
-				'type' => 'array',
-				'meta_type' => 'multiple',
-				'bydefault' => array(
-					'history_time' => array(),
-					'elapsed' => 0,
-				),
-				'history_time' => array(
-					'type' => 'array',
-					'array_type' => 'integer',
-					'meta_type' => 'multiple',
-				),
-				'elapsed' => array(
-					'type' => 'integer',
-					'meta_type' => 'multiple',
-				),
+		);
+
+		$this->model['user_info']['owner_id'] = array(
+			'type'        => 'integer',
+			'meta_type'   => 'multiple',
+			'description' => 'Le responsable de la tâche',
+			'since'       => '1.0.0',
+			'version'     => '1.6.0',
+		);
+
+		$this->model['user_info']['affected_id'] = array(
+			'type'        => 'integer',
+			'meta_type'   => 'multiple',
+			'description' => 'Les utilisateurs (admin) affectés à la tâche',
+			'since'       => '1.0.0',
+			'version'     => '1.6.0',
+		);
+
+		$this->model['user_info']['writer_id'] = array(
+			'type'        => 'integer',
+			'meta_type'   => 'multiple',
+			'description' => 'Plus utilisé depuis 2016.',
+			'since'       => '1.0.0',
+			'version'     => '1.6.0',
+		);
+
+		$this->model['time_info'] = array(
+			'type'      => 'array',
+			'meta_type' => 'multiple',
+			'bydefault' => array(
+				'history_time' => array(),
+				'elapsed'      => 0,
 			),
-			'front_info' => array(
-				'type' => 'array',
-				'meta_type' => 'multiple',
-				'bydefault' => array(
-					'display_time' => false,
-					'display_user' => false,
-					'display_color' => 'white',
-				),
-				'display_time' => array(
-					'type' => 'boolean',
-					'meta_type' => 'multiple',
-				),
-				'display_user' => array(
-					'type' => 'boolean',
-					'meta_type' => 'multiple',
-				),
-				'display_color' => array(
-					'type' => 'string',
-					'meta_type' => 'multiple',
-				),
+		);
+
+		$this->model['time_info']['history_time'] = array(
+			'type'        => 'array',
+			'array_type'  => 'integer',
+			'meta_type'   => 'multiple',
+			'description' => 'L\'ID des historiques de temps.',
+			'todo'        => 'Cet index est sans doute mal nommé (03/01/2018, Jimmy)',
+			'since'       => '1.0.0',
+			'version'     => '1.6.0',
+		);
+
+		$this->model['time_info']['elapsed'] = array(
+			'type'        => 'integer',
+			'meta_type'   => 'multiple',
+			'description' => 'Le temps passé sur la tâche en minute',
+			'since'       => '1.0.0',
+			'version'     => '1.6.0',
+		);
+
+		$this->model['front_info'] = array(
+			'type'        => 'array',
+			'meta_type'   => 'multiple',
+			'description' => 'Les options dans le frontend de WordPress',
+			'todo'        => 'Cette donnée n\'est plus utilisé depuis 2016 (03/01/2018, Jimmy)',
+			'since'       => '1.0.0',
+			'version'     => '1.6.0',
+		);
+
+		$this->model['display_time'] = array(
+			'type'      => 'boolean',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
+
+		$this->model['display_user'] = array(
+			'type'      => 'boolean',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
+
+		$this->model['display_color'] = array(
+			'type'      => 'string',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
+
+		$this->model['task_info'] = array(
+			'type'        => 'array',
+			'meta_type'   => 'multiple',
+			'description' => 'Les informations de la tâche',
+			'since'       => '1.0.0',
+			'version'     => '1.6.0',
+			'bydefault'   => array(
+				'completed'      => false,
+				'order_point_id' => array(),
 			),
-			'task_info' => array(
-				'type' => 'array',
-				'meta_type' => 'multiple',
-				'bydefault' => array(
-					'completed' => false,
-					'order_point_id' => array(),
-				),
-				'completed' => array(
-					'type' => 'boolean',
-					'meta_type' => 'multiple',
-				),
-				'order_point_id' => array(
-					'type' => 'array',
-					'array_type' => 'integer',
-					'meta_type' => 'multiple',
-				),
-			),
-		) );
+		);
+
+		$this->model['task_info']['completed'] = array(
+			'type'      => 'boolean',
+			'meta_type' => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
+
+		$this->model['task_info']['order_point_id'] = array(
+			'type'       => 'array',
+			'array_type' => 'integer',
+			'meta_type'  => 'multiple',
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+		);
 
 		$this->model['taxonomy'] = array(
-			'type' => 'array',
+			'type'      => 'array',
 			'meta_type' => 'multiple',
-			'child' => array(
+			'since'     => '1.0.0',
+			'version'   => '1.6.0',
+			'child'     => array(
 				'wpeo_tag' => array(
-					'meta_type' => 'multiple',
+					'meta_type'  => 'multiple',
 					'array_type' => 'integer',
-					'type' => 'array',
+					'type'       => 'array',
 				),
 			),
 		);
