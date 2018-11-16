3bc0097862488f20eb58a34773273620f452778f
Amandine Eoxia <amandine@eoxia.com>
2018-11-16 16:18:56 +0100

    Modification formulaire create ticket

--- module/support/view/frontend/form-create-ticket.view.php
+++ module/support/view/frontend/form-create-ticket.view.php
@@ -16,12 +16,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 } ?>
 
 <div id="wpeo-window-ask-task" >
-	<form class="form" action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>" method="POST" >
+	<form class="form wpeo-form" action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>" method="POST" >
 		<input type="hidden" name="action" value="create_ticket">
-		<label for="subject"><?php esc_html_e( 'Subject for your request', 'task-manager' ); ?></label>
-		<input id="subject" type="text" name="subject" maxlength="150">
-		<label for="description"><?php esc_html_e( 'A description', 'task-manager' ); ?></label>
-		<textarea id="description" name="description" rows="6"></textarea>
-		<input type="button" data-loader="form" class="action-input" data-parent="form" data-nonce="<?php echo esc_attr( wp_create_nonce( 'create_ticket' ) ); ?>" value="<?php esc_html_e( 'Create ticket', 'task-manager' ); ?>">
+
+		<div class="form-element">
+			<span class="form-label"><?php esc_html_e( 'Subject for your request', 'task-manager' ); ?></span>
+			<label class="form-field-container">
+				<input id="subject" type="text" name="subject" maxlength="150" class="form-field" />
+			</label>
+		</div>
+
+		<div class="form-element">
+			<span class="form-label"><?php esc_html_e( 'A description', 'task-manager' ); ?></span>
+			<label class="form-field-container">
+				<textarea id="description" name="description" rows="6" class="form-field"></textarea>
+			</label>
+		</div>
+
+		<div class="wpeo-button button-main action-input"
+			data-loader="form"
+			data-parent="form"
+			data-nonce="<?php echo esc_attr( wp_create_nonce( 'create_ticket' ) ); ?>">
+
+			<?php esc_html_e( 'Create ticket', 'task-manager' ); ?>
+		</div>
 	</form>
 </div>
