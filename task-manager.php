994d64b64ab433c73af44fc25ab5d8dae650259b
Jimmy <jimmy.eoxia@gmail.com>
2019-11-21 14:58:01 +0100

    Desactive dev mode

--- task-manager.php
+++ task-manager.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 DEFINE( 'PLUGIN_TASK_MANAGER_PATH', realpath( plugin_dir_path( __FILE__ ) ) . '/' );
 DEFINE( 'PLUGIN_TASK_MANAGER_URL', plugins_url( basename( __DIR__ ) ) . '/' );
 DEFINE( 'PLUGIN_TASK_MANAGER_DIR', basename( __DIR__ ) );
-DEFINE( 'PLUGIN_TASK_MANAGER_DEV_MODE', true );
+DEFINE( 'PLUGIN_TASK_MANAGER_DEV_MODE', false );
 
 if ( ! PLUGIN_TASK_MANAGER_DEV_MODE ) {
 	require_once 'core/external/eo-framework/eo-framework.php';
