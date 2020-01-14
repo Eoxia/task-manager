0f027743b655749cb5820b086bfd9ebee277ece9
eoxia-amandine <amandine@eoxia.com>
2020-01-14 10:38:16 +0100

    Ajout différenciation type de contenu

--- module/task/class/task.class.php
+++ module/task/class/task.class.php
@@ -89,7 +89,7 @@ class Task_Class extends \eoxia\Post_Class {
 				'empty' => array(
 					'icon'    => null,
 					'title'   => null,
-					'classes' => 'table-50',
+					'classes' => 'table-75',
 				),
 				'state' => array(
 					'icon'    => 'fas fa-check-square',
