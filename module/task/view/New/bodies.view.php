61c39526b9dcb50f5e5174b79ea93f2ebfcd0280
eoxia-amandine <amandine@eoxia.com>
2020-01-17 16:47:14 +0100

    Modification du lable table row empty

--- module/task/view/New/bodies.view.php
+++ module/task/view/New/bodies.view.php
@@ -32,22 +32,22 @@ if ( ! empty( $contents['bodies'] ) ) :
 else :
 	if ( $parent != null ) :
 		$action = 'edit_point';
-		$text = __( 'Genre pas de tâche, cliquer pour en ajouter une.', 'task-manager' );
+		$text = __( 'No tasks yet. Add a new one', 'task-manager' );
 
 		if ( $parent->data['type'] != 'wpeo-task' ) {
 			$action = 'edit_comment';
-			$text = __( 'Genre pas de commentaire, cliquer pour en ajouter un.', 'task-manager' );
+			$text = __( 'No comment yet. Add a new one', 'task-manager' );
 		}
 
 		?>
-		<div class="table-row">
+		<div class="table-row row-empty">
 			<p><?php echo $text; ?></p>
-			<div class="wpeo-button action-attribute"
+			<div class="wpeo-button action-attribute button-main button-square-30 button-rounded"
 			     data-parent-id="<?php echo $parent->data['id']; ?>"
 			     data-action="<?php echo esc_attr( $action ); ?>"
 			     data-content="<?php esc_html_e( '', 'task-manager' ); ?>"
 			     data-nonce="<?php echo esc_attr( wp_create_nonce( $action ) ); ?>">
-				<i class="fas fa-plus-circle second-icon"></i>
+				<i class="button-icon fas fa-plus-circle second-icon"></i>
 			</div>
 		</div>
 		<?php
