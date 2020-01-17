2f182092927b0c8223b016f21af91b57d355313a
eoxia-amandine <amandine@eoxia.com>
2020-01-17 16:10:45 +0100

    Dropdown horizontal sur les tâches

--- module/task/view/New/render/wpeo_point-empty_add.view.php
+++ module/task/view/New/render/wpeo_point-empty_add.view.php
@@ -1,9 +1,9 @@
 <div class="table-cell-container wpeo-gridlayout grid-2 grid-gap-0">
-	<div class="wpeo-dropdown dropdown-right">
+	<div class="wpeo-dropdown dropdown-horizontal dropdown-right">
 		<div class="dropdown-toggle wpeo-button button-square-50 button-light"><i class="fas fa-ellipsis-v"></i></div>
-		<div class="dropdown-content point-header-action">
+		<ul class="dropdown-content point-header-action">
 			<?php \eoxia\View_Util::exec( 'task-manager', 'point', 'backend/toggle-content', array( 'point' => $data['point'] ) ); ?>
-		</div>
+		</ul>
 	</div>
 
 	<div class="wpeo-button wpeo-tooltip-event button-main button-square-50 action-attribute"
