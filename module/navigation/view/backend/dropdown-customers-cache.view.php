4b64930d2a3d0fb1c050e5615b97749c6f803d54
Jimmy <jimmy.eoxia@gmail.com>
2020-01-23 15:51:17 +0100

    Nowrap

---
+++ module/navigation/view/backend/dropdown-customers-cache.view.php
@@ -0,0 +1,17 @@
+<div class="dropdown-item me item-info">
+	<span class="dropdown-result-title">Please enter three letters</span>
+</div>
+
+<div class="dropdown-item item-nothing me wpeo-util-hidden">
+	<span class="dropdown-result-title">Nothing found</span>
+</div>
+
+		<div class="dropdown-item wpeo-util-hidden" data-content="baa@a.com" data-id="238">
+			<span class="dropdown-result-title">B</span>
+								<span class="dropdown-result-subtitle">a (a@a.com)</span>
+							</div>
+				<div class="dropdown-item wpeo-util-hidden" data-content="aaa@a.com" data-id="184">
+			<span class="dropdown-result-title">a</span>
+								<span class="dropdown-result-subtitle">a (a@a.com)</span>
+							</div>
+		
