e4a8e543f7588254bef63b268ca55ad825c614aa
Jimmy <jimmy.eoxia@gmail.com>
2020-02-06 14:17:24 +0100

    Fix

--- module/notify/class/notify.class.php
+++ module/notify/class/notify.class.php
@@ -135,7 +135,7 @@ class Notify_Class extends \eoxia\Singleton_Util {
 		$time               = strtotime( 'now + 1 hour' ) - strtotime( $notification->time );
 		$notification->time = Task_Class::g()->time_elapsed( $time );
 
-		$notification
+//		$notification
 
 		return $notification;
 	}
